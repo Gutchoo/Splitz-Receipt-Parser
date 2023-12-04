0b4b0b19e1a8e11761ddd06a00e1a518264a22d2
Charles Gutcho <cgutcho@berkeley.edu>
2023-12-04 01:15:09 -0800

    Nanonets V3

--- Nanonets Test.py
+++ Nanonets Test.py
@@ -3,7 +3,7 @@ import requests
 
 url = 'https://app.nanonets.com/api/v2/OCR/Model/08fe2528-e1e6-49c9-aff1-7aae3d13cba5/LabelFile/?async=false'
 
-data = {'file': open('file_path', 'rb')}
+data = {'file': open('file path', 'rb')}
 
 response = requests.post(url, auth=requests.auth.HTTPBasicAuth('API KEY', ''), files=data)
 
@@ -17,8 +17,8 @@ predictions = json_data.get("result", [])[0].get("prediction", [])
 
 # Variables to store extracted data
 merchant_name = ""
-total_amount = ""
-tax_amount = ""
+total_amount = 0.0  
+tax_amount = 0.0    
 date = ""
 items = []
 
@@ -30,9 +30,23 @@ for prediction in predictions:
     if prediction['label'] == 'Merchant_Name':
         merchant_name = prediction['ocr_text']
     elif prediction['label'] == 'Total_Amount':
-        total_amount = prediction['ocr_text']
+        # Clean total
+        clean_total_amount = prediction['ocr_text'].replace('$', '').replace(',', '') if prediction['ocr_text'] else '0.0'
+        try:
+            # print(clean_total_amount, type(clean_total_amount))  # comment out
+            total_amount = float(clean_total_amount)  # truthy value else 0.0
+            # print(total_amount, type(total_amount))  # comment out
+        except ValueError:
+            total_amount = 0.0  # float conversion error
     elif prediction['label'] == 'Tax_Amount':
-        tax_amount = prediction['ocr_text']
+        # Clean tax
+        clean_tax_amount = prediction['ocr_text'].replace('$', '').replace(',', '') if prediction['ocr_text'] else '0.0'
+        try:
+            # print(clean_tax_amount, type(clean_tax_amount))  # comment out
+            tax_amount = float(clean_tax_amount)  # truthy value else 0.0
+            # print(tax_amount, type(tax_amount))  # comment out
+        except ValueError: 
+            tax_amount = 0.0  # float conversion error
     elif prediction['label'] == 'Date':
         date = prediction['ocr_text']
     elif prediction['label'] == 'table':
@@ -41,17 +55,30 @@ for prediction in predictions:
             label = cell['label']
             text = cell['text']
             if row not in item_details:
-                item_details[row] = {'Quantity': '', 'Description': '', 'Line_Amount': ''}
+                item_details[row] = {'Quantity': '1', 'Description': '', 'Line_Amount': ''}  # Default quantity to '1'
             if label in ['Quantity', 'Description', 'Line_Amount']:
                 item_details[row][label] = text
 
 # Convert item details to a list of structured dictionaries
 item_list = []
 for item in item_details.values():
+    # Cleaning line amount
+    clean_line_amount = item['Line_Amount'].replace('$', '').replace(',', '') if item['Line_Amount'] else '0.0'
+    try:
+        price = float(clean_line_amount)
+    except ValueError:
+        price = 0.0
+
+    # Converting quantity to an integer
+    try:
+        quantity = int(item['Quantity']) if item['Quantity'] else 1
+    except ValueError:
+        quantity = 1  # Default to 1 if conversion fails
+
     item_list.append({
         'Name': item['Description'], 
-        'Price': item['Line_Amount'],
-        'Quantity': item['Quantity']
+        'Price': price,
+        'Quantity': quantity
     })
 
 # Final Output Dictionary
@@ -63,5 +90,4 @@ final_output = {
     'Items': item_list
 }
 
-
-# print(final_output)
+print(final_output) # comment out
